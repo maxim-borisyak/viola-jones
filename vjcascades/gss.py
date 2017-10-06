927a10817c1302d471fb72a79e3075043ba0c07a
Maxim Borisyak <mborisyak@yandex-team.ru>
2017-10-06 15:35:04 +0300

    ...

--- vjcascades/gss.py
+++ vjcascades/gss.py
@@ -4,7 +4,8 @@ import theano
 import theano.tensor as T
 
 __all__ = [
-  'golden_section_search'
+  'golden_section_search',
+  'gss'
 ]
 
 def golden_section_search(f, batch_size=128, initial_range=(0, 1)):
@@ -45,3 +46,57 @@ def golden_section_search(f, batch_size=128, initial_range=(0, 1)):
   ]
 
   return init, upd, middle_f, middle
+
+
+def gss(f, steps=64, batch_size=128, initial_range=(0, 1)):
+  """This implementation could be easily speeded up."""
+  golden_ratio = (np.sqrt(5) + 1) / 2
+
+  a, b = initial_range
+
+  left = np.ones(shape=(batch_size,), dtype='float32') * a
+  right = np.ones(shape=(batch_size,), dtype='float32') * b
+
+  delta = (b - a) / golden_ratio
+
+  middle = np.ones(shape=(batch_size,), dtype='float32') * (b - delta / golden_ratio)
+  prob = np.ones(shape=(batch_size,), dtype='float32') * (a + delta / golden_ratio)
+
+  #f_left = f(left)
+  #f_right = f(right)
+  f_middle = f(middle)
+  f_prob = f(prob)
+
+  requests = np.ndarray(shape=(batch_size, ), dtype='float32')
+
+  for i in range(steps):
+    shift_left = f_middle < f_prob
+
+    for j in range(batch_size):
+      if shift_left[j] > 0:
+        right[j] = prob[j]
+        #f_right = f_prob[j]
+        prob[j] = middle[j]
+        f_prob[j] = f_middle[j]
+        middle[j] = right[j] - (right[j] - left[j]) / golden_ratio
+        requests[j] = middle[j]
+      else:
+        left[j] = middle[j]
+        #f_left[j] = f_middle[j]
+
+        middle[j] = prob[j]
+        f_middle[j] = f_prob[j]
+
+        prob[j] = left[j] + (right[j] - left[j]) / golden_ratio
+        requests[j] = prob[j]
+
+      requested = f(requests)
+      for j in range(batch_size):
+        if shift_left[j] > 0:
+          f_middle[j] = requested[j]
+        else:
+          f_prob[j] = requested[j]
+
+
+
+  return
--- vjcascades/numpy_cascades.py
+++ vjcascades/numpy_cascades.py
@@ -1,9 +1,6 @@
 import numpy as np
 
-import theano
-import theano.tensor as T
-
-from gss import golden_section_search
+from gss import gss
 
 def _convolve(cum_images, filters):
   return (
@@ -14,47 +11,45 @@ def _convolve(cum_images, filters):
   )
 
 def _error(y, convs, thresholds, weights):
-  predictions = T.switch(convs > thresholds[None, :], 1, -1)
+  predictions = np.where(convs > thresholds[None, :], 1, -1)
 
   ### mean of weights of miss-classified samples
-  error = T.mean(
-    (1 - y[:, None] * predictions) * weights[:, None] / 2,
-    axis=0
+  error = np.mean(
+    (1 - y[:, None] * predictions) * weights[:, None] / 2, axis=0
   )
 
   return error
 
 def optimal_alpha(y, convs, thresholds, weights):
-  predictions = T.switch(convs > thresholds[None, :], 1, -1)
-  error = T.mean(
-    (1 - y[:, None] * predictions) * weights[:, None] / 2,
-    axis=0
+  predictions = np.where(convs > thresholds[None, :], 1, -1)
+  error = np.mean(
+    (1 - y[:, None] * predictions) * weights[:, None] / 2, axis=0
   )
 
-  correct = T.mean(
+  correct = np.mean(
     (1 + y[:, None] * predictions) * weights[:, None] / 2,
     axis=0
   )
 
-  return 0.5 * (T.log(correct) - T.log(error))
+  return 0.5 * (np.log(correct) - np.log(error))
 
 
 def _predict_ensemble(images, filters, thresholds, alphas):
   conv = _convolve(images, filters)
-  return T.sum(
-    T.switch(conv > thresholds[None, :], 1, -1) * alphas[None, :],
+  return np.sum(
+    np.where(conv > thresholds[None, :], 1, -1) * alphas[None, :],
     axis=1
   )
 
 
 def _stage_predict_ensemble(images, filters, thresholds, alphas):
   conv = _convolve(images, filters)
-  return T.cumsum(
-    T.switch(conv > thresholds[None, :], 1, -1) * alphas[None, :],
+  return np.cumsum(
+    np.where(conv > thresholds[None, :], 1, -1) * alphas[None, :],
     axis=1
   )
 
-class ViolaJonesBoost(object):
+class LightViolaJonesBoost(object):
   def __init__(self,
                n_filters=1024, batch_size=128,
                image_width=28, image_height=28,
@@ -77,87 +72,12 @@ class ViolaJonesBoost(object):
       shape=(n_filters,), dtype='float32'
     )
 
-    X_ = T.ftensor3('X_')
-    X_acc = theano.shared(np.ndarray(shape=(0, image_width, image_height), dtype='float32'))
-    self.X_acc = X_acc
+    self.X = None
 
-    self.set_images = theano.function(
-      inputs=[X_],
-      outputs=None,
-      updates=[
-        (X_acc, T.cumsum(T.cumsum(X_, axis=2), axis=1))
-      ]
-    )
-
-    filters = T.itensor3('filters')
-    thresholds = T.fvector('threshold')
-    alphas = T.fvector('alphas')
-
-    predictions = _predict_ensemble(X_acc, filters, thresholds, alphas)
-    self._predict = theano.function(
-      inputs=[filters, thresholds, alphas],
-      outputs=predictions
-    )
-
-    stage_predictions = _stage_predict_ensemble(X_acc, filters, thresholds, alphas)
-    self._stage_predict = theano.function(
-      inputs=[filters, thresholds, alphas],
-      outputs=stage_predictions
-    )
-
-    ### training
-
-    weights = theano.shared(
-      np.ones(shape=(0,), dtype='float32')
-    )
-    self.weights = weights
-
-    w = T.fvector('weights')
-    self.set_weights = theano.function(
-      inputs=[w],
-      outputs=None,
-      updates=[(weights, w)]
-    )
-
-    y_ = T.fvector('y')
-    y = theano.shared(np.ndarray(shape=(0, ), dtype='float32'))
-    self.set_labels = theano.function(
-      inputs=[y_],
-      outputs=None,
-      updates = [(y, y_)]
-    )
-
-    conv_shared = theano.shared(np.ndarray(shape=(0, n_filters), dtype='float32'))
-    self.conv_shared = conv_shared
-
-    e = lambda thr: _error(y, conv_shared, thr, weights)
-    init_search, upd_search, f, solution = \
-      golden_section_search(e,
-                            batch_size=batch_size,
-                            initial_range=activation_range)
-
-    self.initialize_search = theano.function(
-      inputs=[filters],
-      outputs=None,
-      updates=[
-        (conv_shared, _convolve(X_acc, filters))
-      ] + init_search
-    )
-
-    self.update_thresholds = theano.function(
-      inputs=[],
-      outputs=None,
-      updates=upd_search
-    )
-
-    self.get_thresholds = theano.function(inputs=[], outputs=solution)
-    self.get_errors = theano.function(inputs=[], outputs=f)
-
-    self.get_alpha = theano.function(
-      inputs=[],
-      outputs=optimal_alpha(y, conv_shared, solution, weights)
-    )
-
+  def set_images(self, X):
+    self.X = np.ndarray(X.shape, dtype='float32')
+    np.cumsum(X, axis=1, out=self.X)
+    np.cumsum(self.X, axis=2, out=self.X)
 
   def save(self, path):
     np.savez(path, filters=self.filters, thresholds=self.thresholds, alphas=self.alphas)
